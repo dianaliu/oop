f80ec6dc2dabbd2638b6a61385b5faa415bebfd4
Hernel <hag226@nyu.edu>
2011-11-11 19:13:03 -0500

    Test case

--- testfiles/Test.java
+++ testfiles/Test.java
@@ -1,61 +1,31 @@
-/*
-package xtc;
-package xtc.oop;
 
-public class TestClass {
-	int i = 5;
-}
-public class Run {
-	public static void main() {
+//package xtc;
 
-	}
-}
-*/
+import testdependencies.DepFile1;
+//import testdependencies.cave.DepFile3;
+//import testdependencies.starred.*;
 
-//import xtc.tree.Node;
-//import java.io.*;
-//import java.lang.Comparable;
 
-public class Test {
-  int i = 1;
+public class Test{
+
+  public static void main(String[] args) {
+	int i = testMethod(1);
+	System.out.println(" i = " + i);
+	
+	DepFile1 dep1 = new DepFile1();
+	//DepFile3 dep3 = new DepFile3();
+	int j = dep1.methOneDepFile1(2);
+	System.out.println(" j = " + j);
+  }
   
-  int meth1(int arg){
-	return arg;
+  
+  
+  public static int testMethod(int num){
+  	System.out.println("Method invokation test: Returning an int");
+  	return num;
   }
-  int meth2(int arg){
-	return arg;
-  }
-  int meth3(int arg){
-	return arg;
-  }
-	
-	public static void main(String args[]) {
-		System.out.println("Goodbye, world");
-	}
+  
+ 
 }
 
-class Bar extends Test {
-	long kirim = 0;
-	
-	public Bar() { }
-	
-	public int compareTo( Object o ) {
-		return 0;
-	}
-	
-	Bar crash(Bar b) throws IOException {
-		return null;
-	}
-}
-/*
-public class Thing1 extends Bar {
-}
 
-public class Thing2 extends Object {
-
-}
-
-public class Bar2 extends Thing2 {
-
-}
-*/
