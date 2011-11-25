70ac735f713dccf8cfcfe476d41c9f99850ed827
Diana Liu <dianaliu@nyu.edu>
2011-11-25 14:26:19 -0500

    this makes runnable .cc code

--- Demo.java
+++ Demo.java
@@ -2,15 +2,27 @@
 // String arrays work wtih 10/13
 
 public class Demo {
+    
+    /*
+     * Errors for multiple declarations, but getting closer to overloading!
+    public static int retInt() {
+	int i = 4+5;
+	return i;
+    }
+    
+    */
+
+    public String retS(String s) {
+	return s;
+    }
 
     public static void main (String[] args) {
 
 	Demo d = new Demo();
-	//	System.out.println("d.toString() = " + d.toString());
-
-	// Can only print type Strings lol bc don't know Type in Expressions
-	
 	String s = "soo";
+
+
+	System.out.println("d.retS(s) = " + d.retS(s));
 
 	String[] ss = new String[2];
 	ss[0] = "zero";
@@ -20,10 +32,9 @@ public class Demo {
 	    System.out.println("ss = " + ss[i]);
 	}
 
-	//	System.out.println(ss.getClass());
 	System.out.println("ss[1] = " + ss[1]);
 
-
+	System.out.println("A string s = " + s);
 
 
     }
