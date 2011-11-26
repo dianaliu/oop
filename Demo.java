6842042990ff84b7ce7f6415c17949d1ed1cf9e6
Diana Liu <dianaliu@nyu.edu>
2011-11-25 23:42:49 -0500

    working Demo

--- Demo.java
+++ Demo.java
@@ -17,13 +17,26 @@ public class Demo {
 
     }
 
+    public int retI(int i) {
+	return i + 2;
+    }
+
+    //    public String toString() {
+	// FIXME: Method overriding. Doesn't work
+    //	return "HAHA I OVERRODE YOU";
+    //    }
+
     public static void main (String[] args) {
 
 	Demo d = new Demo();
 	String s = "soo";
 
-
 	System.out.println("d.retS(s) = " + d.retS(s));
+	System.out.println("d.retI(4) = " + d.retI(4));
+	System.out.println("d.retS(s).toString() = " + d.retS(s).toString());
+
+	Object o = d;
+	System.out.println("o class = " + o.getClass().toString());
 
 	String[] ss = new String[2];
 	ss[0] = "zero";
@@ -37,6 +50,17 @@ public class Demo {
 
 	System.out.println("A string s = " + s);
 
+
+	// FIXME: Generate templates for custom arrays if used, 
+	// lookup via symbol table!
+	//	Demo[] dd = new Demo[2];
+	//	dd[0] = new Demo();
+	//	dd[1] = new Demo();
+
+	//	for(int i=0; i < 2; i++) {
+	//	    System.out.println("dd[] = " + dd[i].getClass().toString());
+	//	}
+	
 
     }
 }
