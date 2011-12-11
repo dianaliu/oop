11824c22b6c2f0d71d4ca9e93785a4882604d990
Diana Liu <dianaliu@nyu.edu>
2011-12-10 22:14:41 -0500

    Constructor examples

--- examples/Constructor.java
+++ examples/Constructor.java
@@ -3,18 +3,14 @@
 public class Constructor {
 
     int field;
-
-    public Constructor() {
-    	field = 0;
+    
+    public Constructor () {
+   	field = 0;
     }
     
-    //    public Constructor (int f) {
-    //	field = f;
-    //    }
-
 
     public static void main (String[] args) {
-
+ 
 	Constructor c = new Constructor();
 	System.out.println(c.field);
 
