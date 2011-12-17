520fa7881b65a59c1fa42d8950d1ecdd5c8e2577
Diana Liu <dianaliu@nyu.edu>
2011-12-16 20:21:52 -0500

    Adding test file for simple ass Exceptions

--- tests/Exceptions.java
+++ tests/Exceptions.java
@@ -1,32 +1,16 @@
 public class Exceptions
 {
 
-    static void checkSpelling()
-    {
-	try {
-	    if(true) {
-		throw new Exception("hey");
-	    }
-
-	} catch (Exception e) {
-
-	    System.out.println("exception was: " + e.getMessage());
-	    
-	}
-
-	
-    }
-    
-    
     public static void main (String args[])
     {
 		
 	try {
-		checkSpelling(); 
+	    throw new NullPointerException();
+
 
 	} catch (Exception e) {
 
-		System.out.println("exception was: " + e.getMessage());
+		System.out.println("Succesful error throwing");
 
 	}
     } // end main
